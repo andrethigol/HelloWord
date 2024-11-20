b9db81b76679ee46186f940a800fa15d20247b2f
@andredelira <andre@interface1.com.br>
2024-11-20 08:13:23 -0300

    Adicionar arquivos de projeto.

---
+++ HelloWord/Progran.cs
@@ -0,0 +1,14 @@
+﻿namespace HelloWord;
+ class Progran
+{
+    static void Main()
+    {
+        string texto1 = "A primeira frase.";
+        string texto2 = "Segunda frase";
+
+        string paragrafo = texto1 + " " + texto2;
+
+        Console.WriteLine(paragrafo);
+    }
+
+}
