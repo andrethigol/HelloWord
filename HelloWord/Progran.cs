35b0a09817e65c9a2a5d227c86edf0f4cee75c32
@andredelira <andre@interface1.com.br>
2024-11-20 23:20:36 -0300

    somente um teste

--- HelloWord/Progran.cs
+++ HelloWord/Progran.cs
@@ -9,6 +9,15 @@
         string paragrafo = texto1 + " " + texto2;
 
         Console.WriteLine(paragrafo);
+
+
+        DateOnly dia = new DateOnly(2024, 11, 20);
+        
+        string diaEmTexto = dia.ToLongDateString();
+
+        DateTime dia1 = new DateTime(2024, 11, 20, 08, 34, 20);
+
+        Console.WriteLine(dia1);
     }
 
 }
